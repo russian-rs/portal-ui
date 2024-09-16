56e890f9d6dd1a33143952134ba5ed80e2debae7
Max Aminov <aminovmaksim@gmail.com>
2024-09-16 21:39:11 +0200

    csrf provider

---
+++ src/app/providers/CsrfContext.tsx
@@ -0,0 +1,21 @@
+import { createContext, ReactNode, useEffect, useState } from 'react'
+import { RequestHttp } from '../../shared/http/RequestHttp.tsx'
+import { getCookie } from '../../shared/http/Cookies.tsx'
+
+export const CsrfContext = createContext<String>('')
+
+export const CsrfContextProvider = ({ children }: { children?: ReactNode }) => {
+    const [token, setToken] = useState<string>('')
+
+    useEffect(() => {
+        // Request is dummy, after first POST request, server returns CSRF token in response cookie
+        RequestHttp.post('/csrf').then(
+            (_) => {
+                setToken(getCookie('XSRF-TOKEN'))
+            },
+            (_) => {}
+        )
+    }, [])
+
+    return <CsrfContext.Provider value={token}>{children}</CsrfContext.Provider>
+}
