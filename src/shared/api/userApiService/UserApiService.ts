b0904022a86de37584ede398eca57cd3401b2c13
Max Aminov <aminovmaksim@gmail.com>
2024-09-24 22:31:35 +0200

    fix: import

--- src/shared/api/userApiService/UserApiService.ts
+++ src/shared/api/userApiService/UserApiService.ts
@@ -1,4 +1,4 @@
 import { UserApi } from '@russian-rs/portal-api-axios'
-import { RequestHttp } from 'src/shared/http/RequestHttp.tsx'
+import { RequestHttp } from 'src/shared/http/RequestHttp'
 
 export const UserApiService = new UserApi(undefined, undefined, RequestHttp)
