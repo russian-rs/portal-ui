66806f6a1af48057f7a4c432290ec22f0ff6373e
Max Aminov <aminovmaksim@gmail.com>
2024-09-24 22:59:11 +0200

    fix: node server

--- server.js
+++ server.js
@@ -7,18 +7,21 @@ const app = express()
 
 // Setup view engine
 app.set('view engine', 'jade')
-
 app.use(express.static('dist'))
 
-app.get('/', (_req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'))
+app.all('/api*', (_req, res) => {
+    res.status(502).send('Bad Gateway')
 })
 
 app.get('/health', (_req, res) => {
     res.sendStatus(200)
 })
 
+app.get('*', (_req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'))
+})
+
 const server = app.listen(process.env.PORT || 3000, '0.0.0.0', function () {
     const port = server.address().port
-    console.log('PrivateApp started on port %s', port)
+    console.log('App started on port %s', port)
 })
