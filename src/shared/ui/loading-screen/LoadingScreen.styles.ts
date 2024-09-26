d5f00ed539f34ee9db0bc76b67cd338d8b332488
Max Aminov <aminovmaksim@gmail.com>
2024-09-26 17:25:49 +0200

    feature: loading screen

--- src/shared/ui/loading-screen/LoadingScreen.styles.ts
+++ src/shared/ui/loading-screen/LoadingScreen.styles.ts
@@ -1,14 +1,36 @@
 import styled from 'styled-components'
+import { Center } from '@mantine/core'
 
-export const Section = styled.section`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 100%;
+export const CenterContainer = styled(Center)`
+    height: 100vh;
+`
 
-    img {
-        width: 240px;
-        height: 240px;
+export const StyledImage = styled.img`
+    width: 300px;
+`
+
+export const CustomLoader = styled.div`
+    height: 4px;
+    width: 287px;
+    --c: no-repeat linear-gradient(#3a64e9 0 0);
+    background: var(--c), var(--c), #d7b8fc;
+    background-size: 60% 100%;
+    animation: l16 3s infinite;
+    @keyframes l16 {
+        0% {
+            background-position:
+                -150% 0,
+                -150% 0;
+        }
+        66% {
+            background-position:
+                250% 0,
+                -150% 0;
+        }
+        100% {
+            background-position:
+                250% 0,
+                250% 0;
+        }
     }
 `
