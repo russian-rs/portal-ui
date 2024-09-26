fc8d02730d1036a04c4cf103d65c39aa29447b72
Max Aminov <aminovmaksim@gmail.com>
2024-09-26 17:25:49 +0200

    feature: loading screen

--- src/shared/ui/loading-screen/LoadingScreen.tsx
+++ src/shared/ui/loading-screen/LoadingScreen.tsx
@@ -1,12 +1,31 @@
-import { Section } from './LoadingScreen.styles'
-import { Loader, Title } from '@mantine/core'
+import {
+    CenterContainer,
+    CustomLoader,
+    StyledImage,
+} from './LoadingScreen.styles'
+import { Center, Flex, Image } from '@mantine/core'
+import { useEffect, useState } from 'react'
 
 export function LoadingScreen() {
+    const [visible, setVisible] = useState(false)
+
+    useEffect(() => {
+        const timer = setTimeout(() => setVisible(true), 500)
+        // Clear timeout if the component is unmounted
+        return () => clearTimeout(timer)
+    }, [])
+
+    if (!visible) {
+        return <></>
+    }
+
     return (
-        <Section>
-            <Title order={1}>Портал волонтера</Title>
-            <img src="/resources/app-logo.png" alt={'welcome logo'} />
-            <Loader />
-        </Section>
+        <Center component={CenterContainer}>
+            <Flex justify="center" align="center" direction="column" gap="lg">
+                <CustomLoader />
+                <Image src="/resources/pv_title.png" component={StyledImage} />
+                <CustomLoader />
+            </Flex>
+        </Center>
     )
 }
