492ddcaff00f5c5b07ab62f4209930469840ce93
Max Aminov <aminovmaksim@gmail.com>
2024-09-26 22:26:36 +0200

    feature: 404 page refactor

--- src/pages/notFound/ui/NotFound.styles.ts
+++ src/pages/notFound/ui/NotFound.styles.ts
@@ -1,8 +1,23 @@
-import { Title } from '@mantine/core'
+import { Flex, SimpleGrid, Title, Text } from '@mantine/core'
 import styled from 'styled-components'
+import { Image404 } from 'src/pages/notFound/resources/Image404'
 
 export const Root = styled.div`
     height: 100vh;
+`
+
+export const StyledGrip = styled(SimpleGrid)`
+    position: relative;
+`
+
+export const Content = styled(Flex)`
+    padding-top: 220px;
+    position: relative;
+    z-index: 1;
+
+    @media (max-width: $mantine-breakpoint-sm) {
+        padding-top: 120px;
+    }
 `
 
 export const StyledTitle = styled(Title)`
@@ -11,12 +26,24 @@ export const StyledTitle = styled(Title)`
     margin-bottom: var(--mantine-spacing-md);
 
     @media (max-width: $mantine-breakpoint-sm) {
-        font-size: 32px;
+        font-size: 16px;
     }
+`
+
+export const Description = styled.div`
+    max-width: 600px;
+    margin: var(--mantine-spacing-xl) auto calc(var(--mantine-spacing-xl));
 `
 
 export const StyledButton = styled.button`
     @media (max-width: $mantine-breakpoint-sm) {
         width: 100%;
     }
+`
+
+export const StyledImage404 = styled(Image404)`
+    position: absolute;
+    inset: 0;
+    opacity: 0.75;
+    color: light-dark(var(--mantine-color-gray-1), var(--mantine-color-dark-6));
 `
--- src/pages/notFound/ui/NotFound.tsx
+++ src/pages/notFound/ui/NotFound.tsx
@@ -1,7 +1,22 @@
 import { useHistory } from 'react-router-dom'
-import { Text, SimpleGrid, Button, Image, Container, Flex } from '@mantine/core'
-import image from 'src/pages/notFound/resources/404.png'
-import { Root, StyledButton, StyledTitle } from './NotFound.styles'
+import {
+    Text,
+    SimpleGrid,
+    Button,
+    Container,
+    Flex,
+    Group,
+    Center,
+} from '@mantine/core'
+import {
+    Content,
+    Description,
+    Root,
+    StyledButton,
+    StyledGrip,
+    StyledImage404,
+    StyledTitle,
+} from './NotFound.styles'
 import { locale } from 'src/pages/notFound/lib/locale'
 import { FormattedMessage } from 'react-intl'
 import { useSetDocumentTitleByLocale } from 'src/shared/hooks/useDocumentTitle'
@@ -14,23 +29,34 @@ export const NotFound = () => {
     return (
         <Flex component={Root} align="center">
             <Container>
-                <SimpleGrid cols={{ sm: 2 }}>
-                    <Image src={image} />
-                    <Flex direction="column" justify="center">
-                        <StyledTitle>
+                <SimpleGrid cols={{ sm: 2 }} component={StyledGrip}>
+                    <StyledImage404 />
+                    <Flex
+                        direction="column"
+                        justify="center"
+                        component={Content}
+                    >
+                        <Center component={StyledTitle}>
                             <FormattedMessage id={locale.title} />
-                        </StyledTitle>
-                        <Text c="dimmed" size="lg">
+                        </Center>
+                        <Text
+                            c="dimmed"
+                            size="lg"
+                            ta="center"
+                            component={Description}
+                        >
                             <FormattedMessage id={locale.description} />
                         </Text>
-                        <Button
-                            variant="outline"
-                            mt="xl"
-                            component={StyledButton}
-                            onClick={() => history.push('/')}
-                        >
-                            <FormattedMessage id={locale.homeButton} />
-                        </Button>
+                        <Group justify="center">
+                            <Button
+                                variant="outline"
+                                mt="xl"
+                                component={StyledButton}
+                                onClick={() => history.push('/')}
+                            >
+                                <FormattedMessage id={locale.homeButton} />
+                            </Button>
+                        </Group>
                     </Flex>
                 </SimpleGrid>
             </Container>
