bb69c68bab6405d9e6ada99ac47f5fb147a6c249
Max Aminov <aminovmaksim@gmail.com>
2024-09-26 22:03:26 +0200

    feature: footer copyright

--- src/shared/ui/footer/Footer.styles.ts
+++ src/shared/ui/footer/Footer.styles.ts
@@ -4,4 +4,5 @@ import { Group } from '@mantine/core'
 export const StyledGroup = styled(Group)`
     margin-bottom: 1vh;
     margin-left: 1vh;
+    margin-right: 2vh;
 `
--- src/shared/ui/footer/Footer.tsx
+++ src/shared/ui/footer/Footer.tsx
@@ -2,13 +2,19 @@ import { Group } from '@mantine/core'
 import { ThemeSwitcher } from 'src/shared/ui/theme/ThemeSwitcher'
 import { StyledGroup } from 'src/shared/ui/footer/Footer.styles'
 import { LocaleSwitcher } from 'src/shared/ui/locale/LocaleSwitcher'
+import { FormattedMessage } from 'react-intl'
 
 export const Footer = () => {
     return (
         <>
-            <Group component={StyledGroup}>
-                <ThemeSwitcher />
-                <LocaleSwitcher />
+            <Group component={StyledGroup} grow>
+                <Group>
+                    <ThemeSwitcher />
+                    <LocaleSwitcher />
+                </Group>
+                <Group justify="flex-end">
+                    <FormattedMessage id="footer.copyright" />
+                </Group>
             </Group>
         </>
     )
