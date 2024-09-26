b16278d66ae6e7b8ab985da7835f0411ab81c6f0
Max Aminov <aminovmaksim@gmail.com>
2024-09-26 23:45:13 +0200

    feature: welcome page

--- src/shared/ui/footer/Footer.tsx
+++ src/shared/ui/footer/Footer.tsx
@@ -1,18 +1,19 @@
 import { Group, Text } from '@mantine/core'
 import { ThemeSwitcher } from 'src/shared/ui/theme/ThemeSwitcher'
-import { StyledGroup } from 'src/shared/ui/footer/Footer.styles'
 import { LocaleSwitcher } from 'src/shared/ui/locale/LocaleSwitcher'
 import { FormattedMessage } from 'react-intl'
+import classes from './Footer.module.css'
+import { StyledGroup } from 'src/shared/ui/footer/Footer.styles'
 
 export const Footer = () => {
     return (
         <>
-            <Group component={StyledGroup} grow>
+            <Group grow component={StyledGroup}>
                 <Group>
                     <ThemeSwitcher />
                     <LocaleSwitcher />
                 </Group>
-                <Group justify="flex-end">
+                <Group justify="flex-end" className={classes.copyright}>
                     <Text c="dimmed">
                         <FormattedMessage id="footer.copyright" />
                     </Text>
