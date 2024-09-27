5c7fea22f28722debc1edbb1387cad6406772026
Max Aminov <aminovmaksim@gmail.com>
2024-09-27 17:36:47 +0200

    feature: app shell

--- src/app/Root.tsx
+++ src/app/Root.tsx
@@ -6,9 +6,7 @@ import { LanguageContextProvider } from 'src/app/providers/LocaleContext'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from 'src/shared/constants/Query'
 import { theme } from 'src/shared/ui/theme/CustomMantineTheme'
-import AppHeader from 'src/shared/ui/appHeader/AppHeader'
-import AppBody from 'src/shared/ui/appBody/AppBody'
-import AppFooter from 'src/shared/ui/appFooter/AppFooter'
+import { AppShell } from 'src/shared/ui/appShell/AppShell'
 
 /**
  * Root component
@@ -20,13 +18,11 @@ export const Root = () => {
         <MantineProvider defaultColorScheme="auto" theme={theme}>
             <QueryClientProvider client={queryClient}>
                 <LanguageContextProvider>
-                    <AppHeader />
-                    <AppBody>
+                    <AppShell>
                         <UserContextProvider>
                             <RootRouter />
                         </UserContextProvider>
-                    </AppBody>
-                    <AppFooter />
+                    </AppShell>
                 </LanguageContextProvider>
             </QueryClientProvider>
         </MantineProvider>
