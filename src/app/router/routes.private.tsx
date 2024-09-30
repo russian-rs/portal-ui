3b8ad085a51a0163794a80e8964bb44ccd38c05f
Max Aminov <aminovmaksim@gmail.com>
2024-09-30 19:29:21 +0200

    feature: profile page sample

--- src/app/router/routes.private.tsx
+++ src/app/router/routes.private.tsx
@@ -2,7 +2,7 @@ import { lazy } from "react"
 import { RouteProps } from "react-router-dom"
 
 const Logout = lazy(() => import("src/pages/logout"))
-const Profile = lazy(() => import("src/pages/profile"))
+const Profile = lazy(() => import("src/pages/profile/Profile"))
 
 export const routes: RouteProps[] = [
     {
