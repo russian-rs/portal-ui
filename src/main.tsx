dd1d6742a01295c379abd75435670867509ca9e9
Max Aminov <aminovmaksim@gmail.com>
2024-11-16 15:12:06 +0100

    feature: newrelic

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,14 @@
+import { BrowserAgent } from "@newrelic/browser-agent/loaders/browser-agent"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { Root } from "src/app/Root"
+import { jsConfig } from "src/config/newrelic"
+
+if (process.env.NODE_ENV !== "development") {
+    new BrowserAgent(jsConfig)
+} else {
+    console.log("Local build")
+}
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
