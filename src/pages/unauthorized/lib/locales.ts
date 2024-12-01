bf83e1e7dd83aa436e2bf6cd0353a3ca4ba66f48
Max Aminov <aminovmaksim@gmail.com>
2024-12-01 19:52:40 +0100

    feature: unauthorized page

---
+++ src/pages/unauthorized/lib/locales.ts
@@ -0,0 +1,7 @@
+export const locales = {
+    title: "pages.unauthorized.title",
+    documentTitle: "pages.unauthorized.documentTitle",
+    description: "pages.unauthorized.description",
+    homeButton: "pages.unauthorized.homeButton",
+    backButton: "pages.unauthorized.backButton",
+}
