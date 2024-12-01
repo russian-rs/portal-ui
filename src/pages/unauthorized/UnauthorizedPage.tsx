b9f31233cf94d4bd79c8a21a952408014492df37
Max Aminov <aminovmaksim@gmail.com>
2024-12-01 19:52:40 +0100

    feature: unauthorized page

--- src/pages/unauthorized/UnauthorizedPage.tsx
+++ src/pages/unauthorized/UnauthorizedPage.tsx
@@ -1,7 +1,40 @@
-import { Flex } from "@mantine/core"
+import { Button, Flex, Text } from "@mantine/core"
+import { IconChevronLeft, IconHome, IconLockAccess } from "@tabler/icons-react"
+import { FormattedMessage } from "react-intl"
+import { useHistory } from "react-router-dom"
+import { useSetDocumentTitleByLocale } from "src/shared/hooks/useDocumentTitle"
+import { locales } from "./lib/locales"
+import classes from "./UnauthorizedPage.module.scss"
 
 export const UnauthorizedPage = () => {
-    return <Flex>Unauthorized</Flex>
+    useSetDocumentTitleByLocale(locales.documentTitle)
+
+    const history = useHistory()
+
+    return (
+        <Flex className={classes.root}>
+            <IconLockAccess className={classes.image} />
+            <Text className={classes.title}>
+                <FormattedMessage id={locales.title} />
+            </Text>
+            <Text className={classes.description}>
+                <FormattedMessage id={locales.description} />
+            </Text>
+            <Flex columnGap="sm">
+                <Button
+                    variant="light"
+                    color="cyan"
+                    onClick={() => history.goBack()}
+                    leftSection={<IconChevronLeft size={16} />}
+                >
+                    <FormattedMessage id={locales.backButton} />
+                </Button>
+                <Button variant="light" onClick={() => history.push("/")} leftSection={<IconHome size={16} />}>
+                    <FormattedMessage id={locales.homeButton} />
+                </Button>
+            </Flex>
+        </Flex>
+    )
 }
 
 export default UnauthorizedPage
