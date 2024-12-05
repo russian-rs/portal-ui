c474e866cb52798a563c8aa163d5f1762c1bac76
Max Aminov <aminovmaksim@gmail.com>
2024-12-05 18:04:18 +0100

    feature: user profile birth date

--- src/pages/profile/ui/CommonInfoContainer.tsx
+++ src/pages/profile/ui/CommonInfoContainer.tsx
@@ -1,24 +1,17 @@
 import { Button, Container, Flex } from "@mantine/core"
 import { UserInfoDto } from "@russian-rs/portal-api-axios"
 import { IconBrandTelegram, IconPhone } from "@tabler/icons-react"
+import dayjs from "dayjs"
 import { FormattedMessage } from "react-intl"
 import commonClasses from "src/app/styles/private.module.scss"
 import classes from "src/pages/profile/Profile.module.scss"
 import { PropertyBox } from "src/shared/ui/propertyBox/PropertyBox"
 import { TooltipLocalized } from "src/shared/ui/tooltip/TooltipLocalized"
 
-export const CommonInfoContainer = ({
-    userInfo,
-}: {
-    userInfo: UserInfoDto | undefined
-}) => {
+export const CommonInfoContainer = ({ userInfo }: { userInfo: UserInfoDto | undefined }) => {
     return (
         <Flex direction="column">
-            <PropertyBox
-                name={"pages.profile.props.city"}
-                value={userInfo?.city}
-                className={classes.propertyBox}
-            />
+            <PropertyBox name={"pages.profile.props.city"} value={userInfo?.city} className={classes.propertyBox} />
             <PropertyBox
                 name={"pages.profile.props.address"}
                 value={userInfo?.address}
@@ -26,15 +19,11 @@ export const CommonInfoContainer = ({
             />
             <PropertyBox
                 name={"pages.profile.props.birthDate"}
-                value={userInfo?.birthDate}
+                value={dayjs(userInfo?.birthDate).format("DD MMMM YYYY")}
                 className={classes.propertyBox}
             />
             <Container className={commonClasses.divider} />
-            <PropertyBox
-                name={"pages.profile.props.email"}
-                value={userInfo?.email}
-                className={classes.propertyBox}
-            />
+            <PropertyBox name={"pages.profile.props.email"} value={userInfo?.email} className={classes.propertyBox} />
             <PropertyBox
                 name={"pages.profile.props.telegram"}
                 value={userInfo?.telegram}
@@ -48,15 +37,8 @@ export const CommonInfoContainer = ({
                 icon={<IconPhone size={18} />}
                 className={classes.propertyBox}
             />
-            <TooltipLocalized
-                text="pages.profile.buttons.editTooltip"
-                position="bottom"
-            >
-                <Button
-                    disabled={true}
-                    className={classes.button}
-                    variant="outline"
-                >
+            <TooltipLocalized text="pages.profile.buttons.editTooltip" position="bottom">
+                <Button disabled={true} className={classes.button} variant="outline">
                     <FormattedMessage id={"pages.profile.buttons.edit"} />
                 </Button>
             </TooltipLocalized>
