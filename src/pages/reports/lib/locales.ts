2cd3d01c17e7603bbd66284d86e7098f189f900c
Max Aminov <aminovmaksim@gmail.com>
2024-12-05 21:23:30 +0100

    feature: all reports page

---
+++ src/pages/reports/lib/locales.ts
@@ -0,0 +1,12 @@
+export const locales = {
+    title: "pages.report-list.title",
+    volunteer: "pages.report-list.volunteer",
+    creationDate: "pages.report-list.creation-date",
+    weeks: "pages.report-list.weeks",
+    timeSpent: "pages.report-list.time-spent",
+    status: "pages.report-list.status",
+    taskCount: "pages.report-list.task-count",
+    filesCount: "pages.report-list.files-count",
+    total: "pages.report-list.total",
+    empty: "pages.report-list.empty",
+}
