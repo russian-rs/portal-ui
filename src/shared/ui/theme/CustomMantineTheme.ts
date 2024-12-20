b7a614b74e1a54b8be144531f8f9d079dad7df84
Max Aminov <aminovmaksim@gmail.com>
2024-12-20 17:07:25 +0100

    feature: application avatars

--- src/shared/ui/theme/CustomMantineTheme.ts
+++ src/shared/ui/theme/CustomMantineTheme.ts
@@ -1,4 +1,4 @@
-import { createTheme } from "@mantine/core"
+import { createTheme, DEFAULT_THEME, MantineColor } from "@mantine/core"
 
 export const theme = createTheme({
     fontFamily: "Geologica, sans-serif",
@@ -6,3 +6,19 @@ export const theme = createTheme({
     focusRing: "never",
     defaultRadius: "md",
 })
+
+export const getMantineColor = (input: string | undefined | null): MantineColor => {
+    if (!input) {
+        return "dark"
+    }
+    // Generate a numeric hash from the string
+    const hash = input.split("").reduce((acc, char) => acc + char.charCodeAt(0), 0)
+
+    // Get all color keys from MantineThemeColors
+    const colorKeys = Object.keys(DEFAULT_THEME.colors) as MantineColor[]
+
+    // Map the hash to a color key
+    const colorIndex = hash % colorKeys.length
+
+    return colorKeys[colorIndex]
+}
