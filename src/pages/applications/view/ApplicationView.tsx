0e98450d1575c20189a4cab378c88bb93019d6fd
Max Aminov <aminovmaksim@gmail.com>
2025-03-05 17:55:21 +0100

    feature: contract download

--- src/pages/applications/view/ApplicationView.tsx
+++ src/pages/applications/view/ApplicationView.tsx
@@ -1,4 +1,4 @@
-import { Blockquote, Divider, Flex, Text } from "@mantine/core"
+import { Blockquote, Button, Divider, Flex, Text } from "@mantine/core"
 import { ApplicationDto, ContractDto } from "@russian-rs/portal-api-axios"
 import {
     IconArrowRight,
@@ -7,6 +7,7 @@ import {
     IconBriefcaseOff,
     IconCake,
     IconClock,
+    IconContract,
     IconEPassport,
     IconLanguageHiragana,
     IconLocation,
@@ -22,6 +23,7 @@ import { UserContext } from "src/app/providers/UserContext"
 import { ContractDate } from "src/pages/applications/contract/ContractDate"
 import { defaultApplicationDto } from "src/pages/applications/view/lib/defaults"
 import { PrivateApplicationApiService } from "src/shared/api/applications/PrivateApplicationApiService"
+import generateContractPdf from "src/shared/docs/contract"
 import { setDocumentTitleByString } from "src/shared/hooks/useDocumentTitle"
 import { CopyText } from "src/shared/ui/copyText/CopyText"
 import { LoadingScreen } from "src/shared/ui/loading/LoadingScreen"
@@ -86,17 +88,154 @@ export const ApplicationView = () => {
 
     return (
         <Flex className={classes.root}>
-            <Flex direction="column">
-                <Text className={classes.title}>
-                    <FormattedMessage id={locales.title} />
-                </Text>
-                <Flex columnGap="md" mt={16}>
+            <Text className={classes.title}>
+                <FormattedMessage id={locales.title} />
+            </Text>
+            <Flex className={classes.wrapContainer}>
+                <Flex direction="column" gap="md" className={classes.info}>
+                    <Text className={classes.name} variant="gradient">
+                        {application.name + (application.patronymic ? ` (${application.patronymic})` : "")}
+                    </Text>
+                    <Flex columnGap="xl">
+                        <TextPropertyBox
+                            name={locales.createdAt}
+                            value={dayjs(application.created).format("DD MMMM YYYY, HH:mm")}
+                            icon={<IconClock size={14} />}
+                        />
+                        <TextPropertyBox
+                            name={locales.type}
+                            value={<FormattedMessage id={`common.application-type.${application.type}`} />}
+                        />
+                    </Flex>
+                    <Flex className={classes.fields}>
+                        {application.email && (
+                            <TextPropertyBox
+                                name={locales.email}
+                                value={<CopyText text={application.email} />}
+                                icon={<IconAt size={14} />}
+                            />
+                        )}
+                        {application.phone && (
+                            <TextPropertyBox
+                                name={locales.phone}
+                                value={<CopyText text={application.phone} />}
+                                icon={<IconPhone size={14} />}
+                            />
+                        )}
+                        {application.telegram && (
+                            <TextPropertyBox
+                                name={locales.telegram}
+                                value={application.telegram}
+                                href={`https://t.me/${application.telegram}`}
+                                icon={<IconBrandTelegram size={14} />}
+                            />
+                        )}
+                        {application.birthDate && (
+                            <TextPropertyBox
+                                name={locales.birthDate}
+                                value={dayjs(application.birthDate).format("DD MMM YYYY")}
+                                icon={<IconCake size={14} />}
+                            />
+                        )}
+                        {application.passport && (
+                            <TextPropertyBox
+                                name={locales.passport}
+                                value={<CopyText text={application.passport} />}
+                                icon={<IconEPassport size={14} />}
+                            />
+                        )}
+                        {application.citizenship && (
+                            <TextPropertyBox
+                                name={locales.citizenship}
+                                value={application.citizenship}
+                                icon={<IconWorld size={14} />}
+                            />
+                        )}
+                    </Flex>
+                    <Divider className={classes.divider} />
+                    <Text size="sm" c="dimmed">
+                        {(application.inSerbia
+                            ? intl.formatMessage({ id: locales.inSerbia })
+                            : intl.formatMessage({ id: locales.outSerbia })
+                        )
+                            .concat(" ")
+                            .concat(
+                                application.residenceRequired
+                                    ? intl.formatMessage({ id: locales.residenceRequired })
+                                    : intl.formatMessage({ id: locales.residenceNotRequired })
+                            )}
+                    </Text>
+                    {application.inSerbia && (
+                        <Flex className={classes.fields}>
+                            {application.enterDate && (
+                                <TextPropertyBox
+                                    name={locales.enterDate}
+                                    value={dayjs(application.enterDate).format("DD MMM YYYY")}
+                                    icon={<IconArrowRight size={14} />}
+                                />
+                            )}
+                            {application.address && (
+                                <TextPropertyBox
+                                    name={locales.address}
+                                    value={<CopyText text={application.address} />}
+                                    icon={<IconLocation size={14} />}
+                                />
+                            )}
+                        </Flex>
+                    )}
+                    <Divider className={classes.divider} />
+                    <Flex direction="column" rowGap="md">
+                        {application.occupation && (
+                            <TextPropertyBox name={locales.occupation} value={application.occupation} />
+                        )}
+                    </Flex>
+                    {application.hasExperience && (
+                        <TextPropertyBox
+                            name={locales.experience}
+                            value={<Text className={classes.textCard}>{application.experience}</Text>}
+                        />
+                    )}
+                    {!application.hasExperience && (
+                        <Flex columnGap="sm" align="center">
+                            <IconBriefcaseOff size={16} color="gray" />
+                            <Text size="sm" c="dimmed">
+                                <FormattedMessage id={locales.noExperience} />
+                            </Text>
+                        </Flex>
+                    )}
+                    {application.languages && (
+                        <TextPropertyBox
+                            name={locales.languages}
+                            value={application.languages}
+                            icon={<IconLanguageHiragana size={16} />}
+                        />
+                    )}
+                    {application.skills && (
+                        <TextPropertyBox
+                            name={locales.skills}
+                            value={application.skills}
+                            className={classes.textCard}
+                        />
+                    )}
+                    {application.goal && (
+                        <PropertyBox
+                            name={locales.goal}
+                            value={<Blockquote className={classes.textCard}>{application.goal}</Blockquote>}
+                        />
+                    )}
+                    {application.bio && (
+                        <PropertyBox
+                            name={locales.bio}
+                            value={<Blockquote className={classes.textCard}>{application.bio}</Blockquote>}
+                        />
+                    )}
+                </Flex>
+                <Flex gap="md" mt={16} direction="column" className={classes.controls}>
                     <PropertyBox
                         align="start"
                         name={locales.status}
                         value={
                             <ApplicationStatusSelect
-                                withoutIcon={true}
                                 application={application}
                                 className={classes.statusSelect}
                                 onChange={onStatusChange}
@@ -104,142 +243,28 @@ export const ApplicationView = () => {
                         }
                     />
                     <PropertyBox
-                        align="center"
                         name={locales.contractStart}
-                        value={<ContractDate application={application} onChange={onContractChanged} />}
+                        value={
+                            <ContractDate
+                                application={application}
+                                onChange={onContractChanged}
+                                className={classes.contractDate}
+                            />
+                        }
                     />
+                    <Button
+                        variant="gradient"
+                        rightSection={<IconContract size={14} />}
+                        disabled={application.contract == null}
+                        className={classes.contractGenerate}
+                        onClick={() => {
+                            generateContractPdf(application)
+                        }}
+                    >
+                        Скачать договор
+                    </Button>
                 </Flex>
             </Flex>
-            <Text className={classes.name} variant="gradient">
-                {application.name + (application.patronymic ? ` (${application.patronymic})` : "")}
-            </Text>
-            <Flex columnGap="xl">
-                <TextPropertyBox
-                    name={locales.createdAt}
-                    value={dayjs(application.created).format("DD MMMM YYYY, HH:mm")}
-                    icon={<IconClock size={14} />}
-                />
-                <TextPropertyBox
-                    name={locales.type}
-                    value={<FormattedMessage id={`common.application-type.${application.type}`} />}
-                />
-            </Flex>
-            <Flex className={classes.fields}>
-                {application.email && (
-                    <TextPropertyBox
-                        name={locales.email}
-                        value={<CopyText text={application.email} />}
-                        icon={<IconAt size={14} />}
-                    />
-                )}
-                {application.phone && (
-                    <TextPropertyBox
-                        name={locales.phone}
-                        value={<CopyText text={application.phone} />}
-                        icon={<IconPhone size={14} />}
-                    />
-                )}
-                {application.telegram && (
-                    <TextPropertyBox
-                        name={locales.telegram}
-                        value={application.telegram}
-                        href={`https://t.me/${application.telegram}`}
-                        icon={<IconBrandTelegram size={14} />}
-                    />
-                )}
-                {application.birthDate && (
-                    <TextPropertyBox
-                        name={locales.birthDate}
-                        value={dayjs(application.birthDate).format("DD MMM YYYY")}
-                        icon={<IconCake size={14} />}
-                    />
-                )}
-                {application.passport && (
-                    <TextPropertyBox
-                        name={locales.passport}
-                        value={<CopyText text={application.passport} />}
-                        icon={<IconEPassport size={14} />}
-                    />
-                )}
-                {application.citizenship && (
-                    <TextPropertyBox
-                        name={locales.citizenship}
-                        value={application.citizenship}
-                        icon={<IconWorld size={14} />}
-                    />
-                )}
-            </Flex>
-            <Divider className={classes.divider} />
-            <Text size="sm" c="dimmed">
-                {(application.inSerbia
-                    ? intl.formatMessage({ id: locales.inSerbia })
-                    : intl.formatMessage({ id: locales.outSerbia })
-                )
-                    .concat(" ")
-                    .concat(
-                        application.residenceRequired
-                            ? intl.formatMessage({ id: locales.residenceRequired })
-                            : intl.formatMessage({ id: locales.residenceNotRequired })
-                    )}
-            </Text>
-            {application.inSerbia && (
-                <Flex className={classes.fields}>
-                    {application.enterDate && (
-                        <TextPropertyBox
-                            name={locales.enterDate}
-                            value={dayjs(application.enterDate).format("DD MMM YYYY")}
-                            icon={<IconArrowRight size={14} />}
-                        />
-                    )}
-                    {application.address && (
-                        <TextPropertyBox
-                            name={locales.address}
-                            value={<CopyText text={application.address} />}
-                            icon={<IconLocation size={14} />}
-                        />
-                    )}
-                </Flex>
-            )}
-            <Divider className={classes.divider} />
-            <Flex direction="column" rowGap="md">
-                {application.occupation && <TextPropertyBox name={locales.occupation} value={application.occupation} />}
-            </Flex>
-            {application.hasExperience && (
-                <TextPropertyBox
-                    name={locales.experience}
-                    value={<Text className={classes.textCard}>{application.experience}</Text>}
-                />
-            )}
-            {!application.hasExperience && (
-                <Flex columnGap="sm" align="center">
-                    <IconBriefcaseOff size={16} color="gray" />
-                    <Text size="sm" c="dimmed">
-                        <FormattedMessage id={locales.noExperience} />
-                    </Text>
-                </Flex>
-            )}
-            {application.languages && (
-                <TextPropertyBox
-                    name={locales.languages}
-                    value={application.languages}
-                    icon={<IconLanguageHiragana size={16} />}
-                />
-            )}
-            {application.skills && (
-                <TextPropertyBox name={locales.skills} value={application.skills} className={classes.textCard} />
-            )}
-            {application.goal && (
-                <PropertyBox
-                    name={locales.goal}
-                    value={<Blockquote className={classes.textCard}>{application.goal}</Blockquote>}
-                />
-            )}
-            {application.bio && (
-                <PropertyBox
-                    name={locales.bio}
-                    value={<Blockquote className={classes.textCard}>{application.bio}</Blockquote>}
-                />
-            )}
         </Flex>
     )
 }
