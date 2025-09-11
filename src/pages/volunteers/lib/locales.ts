ec9d067663d3dc0916127bded2c9987b7b8c8566
ValeriiLindenPy <v.linden@mail.ru>
2025-09-11 14:15:29 +0200

    feat: add api UI in MintrudReport.tsx

--- src/pages/volunteers/lib/locales.ts
+++ src/pages/volunteers/lib/locales.ts
@@ -1,4 +1,35 @@
 export const locales = {
     titleReports: "pages.volunteers-reports.title",
-    titleMintrud: "pages.volunteers-mintrud.title",
-}
+    titleMintrud: "pages.mintrud.title",
+
+    yearLabel: "pages.mintrud.year-label",
+    summary: "pages.mintrud.summary",
+    totalVolunteers: "pages.mintrud.total-volunteers",
+    totalHours: "pages.mintrud.total-hours",
+
+    programStats: "pages.mintrud.program-stats",
+    program: "pages.mintrud.program",
+    volunteers: "pages.mintrud.volunteers",
+    hours: "pages.mintrud.hours",
+    other: "pages.mintrud.other",
+
+    volunteerStats: "pages.mintrud.volunteer-stats",
+    genderFemale: "pages.mintrud.gender.female",
+    genderMale: "pages.mintrud.gender.male",
+    age15to18: "pages.mintrud.age.15-18",
+    age18to30: "pages.mintrud.age.18-30",
+    age30to40: "pages.mintrud.age.30-40",
+    age40to65: "pages.mintrud.age.40-65",
+    age65AndAbove: "pages.mintrud.age.65-plus",
+
+    finalUsersStats: "pages.mintrud.final-users-stats",
+    culturalAssets: "pages.mintrud.final-users.cultural-assets",
+    naturalAssets: "pages.mintrud.final-users.natural-assets",
+    publicAreas: "pages.mintrud.final-users.public-areas",
+    finalOther: "pages.mintrud.final-users.other",
+    total: "pages.mintrud.total",
+
+    empty: "pages.mintrud.empty",
+};
+
+
