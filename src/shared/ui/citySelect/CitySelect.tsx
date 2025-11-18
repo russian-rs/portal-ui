9f54cac93c77805950185af0e50b5156c81e399a
lalexweb <lalexweb31@gmail.com>
2025-11-18 10:17:35 +0100

    Add city and postal code to other places which were missed, update CitySelect component

--- src/shared/ui/citySelect/CitySelect.tsx
+++ src/shared/ui/citySelect/CitySelect.tsx
@@ -1,6 +1,6 @@
 import { Autocomplete, AutocompleteProps } from "@mantine/core"
 import { useQuery } from "@tanstack/react-query"
-import { useMemo, useState } from "react"
+import { useCallback, useEffect, useMemo, useRef, useState } from "react"
 import { useIntl } from "react-intl"
 import { CitiesApiService } from "src/shared/api/CitiesApiService"
 
@@ -8,9 +8,11 @@ const isCyrillic = (text: string): boolean => {
     return /[\u0400-\u04FF]/.test(text)
 }
 
-export const CitySelect = ({ onChange, ...props }: AutocompleteProps) => {
+export const CitySelect = ({ onChange, value, ...props }: AutocompleteProps) => {
     const intl = useIntl()
     const [search, setSearch] = useState<string>("")
+
+    const skipSearchRef = useRef<boolean>(false)
 
     const { data: cities = [] } = useQuery({
         queryKey: ["cities"],
@@ -21,24 +23,41 @@ export const CitySelect = ({ onChange, ...props }: AutocompleteProps) => {
         return cities.map((city) => (isCyrillic(search) ? city.nameCyrillic : city.name))
     }, [cities, search])
 
-    const handleChange = (newValue: string) => {
-        setSearch(newValue)
-        const city = cities.find((c) => c.name === newValue || c.nameCyrillic === newValue)
+    const handleChange = useCallback(
+        (newValue: string) => {
+            if (!cities.length) return
 
-        if (city) {
-            onChange?.(city.name)
-        } else {
-            onChange?.("")
+            setSearch(newValue)
+            const city = cities.find((c) => c.name === newValue || c.nameCyrillic === newValue)
+
+            if (city) {
+                onChange?.(city.name)
+                skipSearchRef.current = true
+            } else {
+                onChange?.("")
+            }
+        },
+        [cities, onChange]
+    )
+
+    useEffect(() => {
+        if (!value || value === search) return
+
+        if (skipSearchRef.current) {
+            skipSearchRef.current = false
+            return
         }
-    }
+
+        handleChange(value)
+    }, [value, search, handleChange])
 
     return (
         <Autocomplete
-            placeholder={intl.formatMessage({ id: "common.city-select.placeholder" })}
-            data={cityOptions}
-            value={search}
-            onChange={handleChange}
             {...props}
+            placeholder={intl.formatMessage({ id: "common.city-select.placeholder" })}
+            value={search}
+            data={cityOptions}
+            onChange={handleChange}
         />
     )
 }
