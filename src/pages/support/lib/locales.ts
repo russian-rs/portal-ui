b34c5fa54bffb2b615e07f541675f22f217a5f19
Маргарита Митяговская <margaritamitiagovskaia@MacBook-Air-Margarita.local>
2025-12-12 13:18:42 +0100

    support first version

---
+++ src/pages/support/lib/locales.ts
@@ -0,0 +1,6 @@
+export const locales = {
+    title: "pages.support.documentTitle",
+    heading: "pages.support.title",
+    description: "pages.support.description",
+    button: "pages.support.createButton",
+}
