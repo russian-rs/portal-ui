626620f1644a4e1ad692f59a649dbb5970d8cb53
lalexweb <lalexweb31@gmail.com>
2026-01-06 14:17:26 +0100

    migrate eslint to a new version

---
+++ eslint.config.js
@@ -0,0 +1,75 @@
+import js from "@eslint/js"
+import tseslint from "typescript-eslint"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import react from "eslint-plugin-react"
+import prettier from "eslint-config-prettier"
+import globals from "globals"
+
+export default tseslint.config(
+    {
+        ignores: [
+            "**/node_modules",
+            "**/dist",
+            "cypress",
+            "src/**/*.test.ts",
+            "src/**/*.test.tsx",
+            "src/stories",
+        ],
+    },
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        files: ["**/*.{ts,tsx}"],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: {
+                ...globals.node,
+                ...globals.browser,
+                ...globals.es2020,
+            },
+            parserOptions: {
+                project: ["./tsconfig.json"],
+                warnOnUnsupportedTypeScriptVersion: true,
+            },
+        },
+        plugins: {
+            react,
+            "react-hooks": reactHooks,
+            "react-refresh": reactRefresh,
+        },
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+        rules: {
+            ...react.configs.recommended.rules,
+            ...reactHooks.configs.recommended.rules,
+            "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+            "no-unused-vars": "off",
+            "@typescript-eslint/no-unused-vars": [
+                "warn",
+                {
+                    args: "none",
+                    ignoreRestSiblings: true,
+                },
+            ],
+            "@typescript-eslint/dot-notation": [
+                "error",
+                {
+                    allowIndexSignaturePropertyAccess: true,
+                },
+            ],
+            "@typescript-eslint/no-use-before-define": "off",
+        },
+    },
+    {
+        files: ["src/**/*.tsx"],
+        rules: {
+            "@typescript-eslint/no-use-before-define": "off",
+        },
+    },
+    prettier
+)
+
