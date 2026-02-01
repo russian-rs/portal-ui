1748e6113981951a2af9841978446ac52b06f0ce
digiben <serg-rammstein@mail.ru>
2026-02-01 14:11:04 +0100

    feature: insert new roles (#45)

--- src/pages/cleaning/lib/roles.ts
+++ src/pages/cleaning/lib/roles.ts
@@ -1,7 +1,7 @@
 import { UserInfoDto } from "@russian-rs/portal-api-axios"
 import { hasPermission } from "src/shared/user/roles"
 
-const allowed = ["MAIN_VOLUNTEER", "ADMIN_VOLUNTEER", "ADMIN"]
+const allowed = ["VOLUNTEER_CLEAN_CITY","MAIN_VOLUNTEER", "ADMIN_VOLUNTEER", "ADMIN"]
 
 export const hasAccess = (user: UserInfoDto | null): boolean => {
     return hasPermission(user, allowed)
