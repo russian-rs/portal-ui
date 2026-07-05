21e29ba196f62ac69172a713c78d3159e3e29b85
ValeriiLindenPy <valeriilinden@gmail.com>
2026-07-05 15:37:57 +0200

    fix: public access to program/project selectors

--- src/app/router/routes.public.tsx
+++ src/app/router/routes.public.tsx
@@ -2,6 +2,8 @@ import { lazy } from "react"
 import { RouteProps } from "react-router"
 import { ProgramsProvider } from "src/app/providers/ProgramsProvider"
 import { ProjectsProvider } from "src/app/providers/ProjectsProvider"
+import { PublicProgramsApiService } from "src/shared/api/PublicProgramsApiService"
+import { PublicProjectsApiService } from "src/shared/api/PublicProjectsApiService"
 
 const Welcome = lazy(() => import("src/pages/welcome/Welcome"))
 const Application = lazy(() => import("src/pages/application/Application"))
@@ -16,8 +18,8 @@ export const routes: RouteProps[] = [
     {
         path: "/application/form",
         element: (
-            <ProgramsProvider>
-                <ProjectsProvider>
+            <ProgramsProvider apiService={PublicProgramsApiService}>
+                <ProjectsProvider apiService={PublicProjectsApiService}>
                     <ApplicationForm />
                 </ProjectsProvider>
             </ProgramsProvider>
